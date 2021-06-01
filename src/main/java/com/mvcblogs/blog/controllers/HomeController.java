b47438e25472038d7d4213c4d30d5e281cbcadff
Daniel Correa <yo@danielgara.com>
2021-06-01 12:11:36 -0500

    imp: style css

--- src/main/java/com/mvcblogs/blog/controllers/HomeController.java
+++ src/main/java/com/mvcblogs/blog/controllers/HomeController.java
@@ -1,18 +1,23 @@
 package com.mvcblogs.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("title", "Blog - Spring");
+		model.addAttribute("description", "A clean blog with an MVC architecture in Spring");
 		return "home/index";
 	}
 	
 	@GetMapping("/about")
-	public String about() {
+	public String about(Model model) {
+		model.addAttribute("title", "About us");
+		model.addAttribute("description", "Information about the developers of this application");
 		return "home/about";
 	}
 }
